f815cc3b322c6c7d805cd2215f23dff1656b8726
Tianle Li <117613371+Tianle-Li@users.noreply.github.com>
2026-02-25 17:00:35 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,12 +19,10 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'y'
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-extensions = [
-    'sphinx_rtd_theme',
-]
+
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
